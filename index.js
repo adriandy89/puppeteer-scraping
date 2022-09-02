c6dd6ced6e77ffb9293833410c51900f7b258395
Adrian DY <adrian.dy89@gmail.com>
2022-09-01 22:51:45 -0400

    * Update [get items from amazon]

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const puppeteer = require('puppeteer');
     try {
         const browser = await puppeteer.launch({
             args: ["--no-sandbox"],
+            // headless - abrir o cerrar el navegador
             headless: true,
             ignoreHTTPSErrors: true
           });
@@ -12,8 +13,41 @@ const puppeteer = require('puppeteer');
         // disable default timeout
         await page.setDefaultNavigationTimeout(0);
 
-        await page.goto('https://pptr.dev/')
-        await page.screenshot({ path: './download/puppeteer1.jpg' })
+        await page.goto('https://www.amazon.es/')
+
+        await page.waitForSelector('[id=sp-cc-accept]')
+        await page.click('#sp-cc-accept')
+        
+        await page.screenshot({ path: './download/puppeteer1.jpg' })  
+
+        await page.waitForSelector('[id=twotabsearchtextbox]')
+        await page.type('#twotabsearchtextbox', 'core i7')
+        await page.screenshot({ path: './download/puppeteer2.jpg' })        
+        
+        await page.click('#nav-search-submit-button')
+        await page.waitForSelector('[data-component-type=s-search-result]')
+        await page.screenshot({ path: './download/puppeteer3.jpg' })
+
+        const linksArray = await page.evaluate(() => {
+            const elements = document.querySelectorAll('[data-component-type=s-search-result] h2 a')
+            const links = []
+            elements.forEach(element => {
+                links.push(element.href)
+            });
+            return links;
+        })
+        console.log(linksArray);
+        const ranks = []
+        for (const link of linksArray) {
+            await page.goto(link)
+            await page.waitForSelector('#productTitle')
+            const rank = await page.evaluate(() => {
+                const element = document.querySelector('#productTitle')
+                return element.textContent;
+            })
+            ranks.push({rank})
+        }
+        console.log(ranks);
 
         await browser.close()
     } catch (error) {
